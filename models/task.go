993a4451d03bd1adb193b87a127d219885746b5a
Aman Patel <57236310+asrp04@users.noreply.github.com>
2026-09-25 18:26:08 +0530

    Update task.go

--- models/task.go
+++ models/task.go
@@ -3,7 +3,7 @@ package models
 import "gorm.io/gorm"
 
 type Task struct {
-	gorm.Model        // यह ID, CreatedAt, UpdatedAt, DeletedAt अपने आप देगा
+	gorm.Model   
 	Title       string `json:"title" gorm:"not null"`
 	Description string `json:"description"`
 	Done        bool   `json:"done" gorm:"default:false"`
