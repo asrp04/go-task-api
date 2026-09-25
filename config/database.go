3a9651e4798ba8d6ef332d93acbde9039fab65f4
Aman Patel <57236310+asrp04@users.noreply.github.com>
2026-09-25 18:30:44 +0530

    Update database.go

--- config/database.go
+++ config/database.go
@@ -13,30 +13,28 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// .env फाइल को लोड कर रहे हैं
+	
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Warning: .env file not found, using system env")
 	}
 
-	// os.Getenv के जरिए .env से वैल्यूज निकालना
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	// डायनामिक DSN स्ट्रिंग बनाना
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		dbUser, dbPassword, dbHost, dbPort, dbName)
 
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("डेटाबेस से कनेक्ट करने में विफल!")
+		panic("Failed connecting to database!")
 	}
 
 	database.AutoMigrate(&models.Task{}, &models.User{})
 
-	fmt.Println("डेटाबेस कनेक्शन सफल रहा (.env लोड हो गया)!")
+	fmt.Println("Database connection is successfull (.env is loaded)!")
 	DB = database
 }
